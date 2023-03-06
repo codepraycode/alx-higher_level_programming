7000ac3cd375c4330884f22f6b6a49d8c475adcc
Precious Olusola <preciousolusola16@gmail.com>
2023-03-06 08:04:19 -0800

    Updated last submission

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax".split()
-print(str[5], str[6], end=" ")
-print(str[12], str[0])
+# just an empty line, i don't know what to do here.
+print(str[5], str[6], str[12], str[0])
