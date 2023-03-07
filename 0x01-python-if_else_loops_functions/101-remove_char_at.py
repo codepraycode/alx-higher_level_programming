03b77aa5eb3af9792edfbc454ff06814ee525f8e
Precious Olusola <preciousolusola16@gmail.com>
2023-03-07 04:03:16 -0800

    Done with second advance task

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(string, n):
-    return string[:n] + string[n+1:]
+    new_str = ""
+    for chi in range(len(string)):
+        if chi != n:
+            new_str += string[chi]
+    return new_str
