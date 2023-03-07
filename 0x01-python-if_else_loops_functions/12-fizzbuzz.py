4e6aa0586ad98a7f96dda22aad10636aa504a722
Precious Olusola <preciousolusola16@gmail.com>
2023-03-07 03:09:15 -0800

    Done with thirteenth task

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(1,101):
+        res = i
+        if (i % 3 == 0) and (i % 5 == 0):
+            res = "FizzBuzz"
+        elif i % 3 == 0:
+            res = "Fizz"
+        elif i % 5 == 0:
+            res = "Buzz"
+        print(res, end=" ")
