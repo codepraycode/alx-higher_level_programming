9a28f3103b41e9ca74fd19e4b094e4bb7d3ce328
Precious Olusola <preciousolusola16@gmail.com>
2023-03-07 03:51:13 -0800

    Updated last submission

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for ch in range(65+25, 64, -1):
-    print(f"{chr(ch + 32 if (ch % 2 == 0) else ch)}", end="")
+for ch in reversed(range(97, 123)):
+    print("{:c}".format(ch if (ch % 2 == 0) else (ch - 32)), end='')
