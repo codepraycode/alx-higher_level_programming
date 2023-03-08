c60b4e5d1afa74b4c1d0fc43d972de9ea9fb2270
Precious Olusola <51521181+codepraycode@users.noreply.github.com>
2023-03-08 22:34:16 +0100

    Update 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 for ch in range(26):
-  # ignore for e and q
-  if ch == 4 or ch == 16: continue
-  print("{:s}".format(chr(ch + ord('a'))), end="")
+    if ch != 4 and ch != 16: # ignore for e and q
+        print("{:s}".format(chr(ch + ord('a'))), end="")
