781f9f7bd9becc5ca2de677f545f6ac58890152e
Precious Olusola <51521181+codepraycode@users.noreply.github.com>
2023-03-28 06:59:39 +0100

    Updated last submission

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    number_of_real_elements = 0
     for e in my_list[:x]:
+        number_of_real_elements += 1
         print(e, end="")
     print()
+    return number_of_real_elements
