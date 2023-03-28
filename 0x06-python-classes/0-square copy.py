f6ec5a32f81d4f71b0ca0fda15ad3c0061ee6b2b
Precious Olusola <51521181+codepraycode@users.noreply.github.com>
2023-03-28 08:06:34 +0100

    Done with next task

---
+++ 0x06-python-classes/0-square copy.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+    Square class with size
+"""
+
+
+class Square(object):
+    """
+        A Square class to be instantiated with a size
+    """
+
+    def __init__(self, size):
+        """
+            Initializing class and constructing square
+        """
+        self.__size = size
