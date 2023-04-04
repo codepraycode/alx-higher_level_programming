a5cc3f54430e26ffc342677dd945c5111f065696
Precious Olusola <51521181+codepraycode@users.noreply.github.com>
2023-04-04 11:37:49 +0100

    Done with first task

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Function to add integers
+    Prototye: def add_integer(a, b=98)
+
+        Args:
+            a(int, float): First integer
+            b(int, float, optional): second integer
+
+        Raises:
+            TypeError: if a or b is not an integer or float
+                expected message should be:
+                `a must be an integer` or `b must be an integer`
+
+        Returns:
+            int: the addition of given arguments
+"""
+
+
+def add_integer(a, b=98):
+
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    # Cast arguments to integers
+    a = int(a)
+    b = int(b)
+
+    return a + b
