de1c8a75fcfc2e387f914a9feb8231b35399353c
Precious Olusola <51521181+codepraycode@users.noreply.github.com>
2023-04-13 16:56:42 +0100

    Done with task 7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""A script that adds all arguments to a Python list,
+    and then save them to a file
+"""
+import sys
+from os import path
+
+
+# Load functions from other modules
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+# CONSTANTS: File name for export
+FILE_NAME = "add_item.json"
+
+# Length of terminal argument
+_arg_count = len(sys.argv)
+
+# list: List of files
+file_lists = []
+
+if path.exist(filename):
+    file_lists = load_from_json_file(FILE_NAME)
+
+if _arg_count == 1:
+    save_to_json_file(file_lists, FILE_NAME)
+else:
+    for each_i in range(1, _arg_count):
+        file_lists.append(sys.argv[each_i])
+
+    save_to_json_file(file_lists, FILE_NAME)
