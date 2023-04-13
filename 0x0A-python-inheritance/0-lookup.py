5dfda6d4f312e8765535ce9438623f3e7b4ffd9a
Precious Olusola <51521181+codepraycode@users.noreply.github.com>
2023-04-13 14:55:44 +0100

    Done with task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Function to get a list of all available attributes and methods
+    of an object
+
+    Prototype: def lookup(obj)
+        Args:
+            obj: object to lookup
+        Returns:
+            list: a list of available attributes and methods
+"""
+
+def lookup(obj):
+    """A function that returns a list of available attributes
+    and methods of an object
+
+    Args:
+        obj: an object instance of a class
+
+    Returns:
+        list: a list of available attributes and methods of `obj`
+    """
+    return dir(obj)
