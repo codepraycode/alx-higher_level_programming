665abf6c90ba8ee5e253f1635c66f964c3c7d658
Precious Olusola <51521181+codepraycode@users.noreply.github.com>
2023-04-13 16:16:11 +0100

    Done with task 101

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Function to add attribute to an object if possible,
+    otherwise throws exception.
+
+    Prototype: def add_attribute(obj, key, value)
+        Args:
+            obj: object to add attribute to
+            key(str): name of the attribute
+            value(any): value to be associated with `key`
+
+        Raises:
+            TypeError: if the object can't have new attribute
+                exception message could be `can't add new attribute`
+"""
+
+
+def add_attribute(obj):
+    """Function to add attribute to an object if possible,
+    otherwise throws exception.
+
+    Args:
+        obj: object to add attribute to
+        key(str): name of the attribute
+        value(any): value to be associated with `key`
+
+    Raises:
+        TypeError: if the object can't have new attribute
+            exception message could be `can't add new attribute`
+    """
+    
+    if not hasattr(obj, '__dict__'):
+        raise TypeError("can't add new attribute")
+    
+    setattr(obj, key, value)
