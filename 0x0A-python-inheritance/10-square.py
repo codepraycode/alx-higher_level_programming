8d393cdbb4f341612f9a6f97073d621d025bc401
Precious Olusola <51521181+codepraycode@users.noreply.github.com>
2023-04-13 15:54:58 +0100

    Done with task 10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Square  class, a sub-class of Rectangle class"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Square(Rectangle):
+    """Square class, a sub-class of Rectangle class"""
+
+    def __init__(self, size):
+        """Initialize an instance with width and height
+
+        Notes: `size` will be validated by `integer_validator`
+            from `BaseGeometry`.
+
+        Args:
+            size(int): size of Square instance
+        """
+
+        BaseGeometry.integer_validator(self, 'size', size)
+        self.__size = size
+
+    def area(self):
+        """Returns a calculated area of the instance"""
+        return self.__width * 2
