ade4e303c5f37c1fe248ab0864db98ec4e4b126d
Precious Olusola <51521181+codepraycode@users.noreply.github.com>
2023-04-13 16:42:14 +0100

    Updated task 5: fixed pep

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -10,7 +10,8 @@ import json
 
 
 def save_to_json_file(my_obj, filename):
-    """Function that writes an Object to a text file, using a JSON representation
+    """Function that writes an Object to a text file,
+    using a JSON representation
 
     Args:
         my_obj(dict): a dictionary object
