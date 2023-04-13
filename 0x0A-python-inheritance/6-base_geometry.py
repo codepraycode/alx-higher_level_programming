50a7f1500f9ed1da7681f24e01d4a2dace90cbe3
Precious Olusola <51521181+codepraycode@users.noreply.github.com>
2023-04-13 15:22:21 +0100

    Done with task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""BaseGeometry class"""
+
+
+class BaseGeometry(object):
+    """BaseGeometry class"""
+
+    def area(self):
+        """A method to define the area of the geometry instance"""
+        raise Exception("area() is not implemented")
