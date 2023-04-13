7089c59ba99ebc0f44281387bb3f07c60a093d3a
Precious Olusola <51521181+codepraycode@users.noreply.github.com>
2023-04-13 16:33:24 +0100

    Done with task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Function that returns the JSON representation of an object (string)
+
+    Prototype: def to_json_string(my_obj):
+        Args:
+            my_obj(str): object string
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """Function that returns the JSON representation of an object (string)
+
+    Args:
+        my_obj(str): object string
+    """
+
+    return json.dumps(obj)
