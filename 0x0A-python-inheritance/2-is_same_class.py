fdea0020e5636decdfb2f596d5f59e351ea942e5
Precious Olusola <51521181+codepraycode@users.noreply.github.com>
2023-04-13 15:07:21 +0100

    Done with task 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Function that returns True if the
+    object is exactly an instance of the specified class; otherwise False.
+
+    Prototype: def is_same_class(obj, a_class)
+        Args:
+            obj: object to check
+            a_class: class used to check object
+        Returns:
+            bool: returns True if the object is exactly an instance
+            of the `a_class`; otherwise False.
+"""
+
+
+def is_same_class(obj, a_class):
+    """Function that returns True if the 
+    object is exactly an instance of the specified class; otherwise False.
+
+    Args:
+        obj: object to check
+        a_class: class used to check object
+    Returns:
+        bool: returns True if the object is exactly an instance 
+        of the `a_class`; otherwise False.
+    """
+    return isinstance(obj, a_class)
