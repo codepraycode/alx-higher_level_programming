5a45002dc8d6149fe057e3022f403fd3fea076e6
Precious Olusola <51521181+codepraycode@users.noreply.github.com>
2023-04-13 15:37:37 +0100

    Done with task 8

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Rectangle class, a sub-class of BaseGeometry class"""
+import BaseGeometry from 7-base_geometry
+
+class Rectangle(BaseGeometry):
+    """Rectangle class, a sub-class of BaseGeometry class"""
+
+    def __init__(self, width, height):
+        """Initialize an instance with width and height
+
+        Notes: `width` and `height` will be validated by `integer_validator`
+            from `BaseGeometry`.
+
+        Args:
+            width(int): Width of rectangle instance
+            height(int): Height of rectangle instance
+        
+        """
+
+        if self.integer_validator('width', width):
+            self.__width = width
+
+        if self.integer_validator('height', height):
+            self.__height = height
