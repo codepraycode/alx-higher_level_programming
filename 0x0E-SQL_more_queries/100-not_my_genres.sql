9d1a9b315e5eceb57e6a44a587114a5c01a31063
Precious Olusola <51521181+codepraycode@users.noreply.github.com>
2023-05-19 03:59:42 +0100

    Updated advacnce task 100

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,6 +1,20 @@
--- lists all shows in hbtn_0d_tvsows that have at least 1 genre linked
-SELECT tv_shows.title, tv_show_genres.genre_id
-       FROM tv_shows
-       INNER JOIN tv_show_genres
-       ON tv_shows.id = tv_show_genres.show_id
-       ORDER BY tv_shows.title, tv_show_genres.genre_id;
+-- Lists all genres of the database hbtn_0d_tvshows
+-- not linked to the show Dexter.
+-- Records are sorted by ascending genre name.
+SELECT DISTINCT `name`
+  FROM `tv_genres` AS g
+       INNER JOIN `tv_show_genres` AS s
+       ON g.`id` = s.`genre_id`
+
+       INNER JOIN `tv_shows` AS t
+       ON s.`show_id` = t.`id`
+       WHERE g.`name` NOT IN
+             (SELECT `name`
+                FROM `tv_genres` AS g
+	             INNER JOIN `tv_show_genres` AS s
+		     ON g.`id` = s.`genre_id`
+
+		     INNER JOIN `tv_shows` AS t
+		     ON s.`show_id` = t.`id`
+		     WHERE t.`title` = "Dexter")
+ ORDER BY g.`name`;
