5c87b1f6dcfac3d40590b5fff858c979e5aac228
Precious Olusola <51521181+codepraycode@users.noreply.github.com>
2023-05-19 03:59:21 +0100

    Updated task 13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,11 @@
--- lists all shows in hbtn_0d_tvsows that have at least 1 genre linked
-SELECT tv_shows.title, tv_show_genres.genre_id
-       FROM tv_shows
-       LEFT OUTER JOIN tv_show_genres
-       ON tv_shows.id = tv_show_genres.show_id
-       WHERE tv_show_genres.show_id IS NULL
-       ORDER BY tv_shows.title, tv_show_genres.genre_id;
+-- Lists all genres from the database hbtn_0d_tvshows along with the number of
+-- shows linked to each.
+-- Does not display genres without linked shows.
+-- Records are ordered by descending number of shows linked.
+SELECT g.`name` AS `genre`,
+       COUNT(*) AS `number_of_shows`
+  FROM `tv_genres` AS g
+       INNER JOIN `tv_show_genres` AS t
+       ON g.`id` = t.`genre_id`
+ GROUP BY g.`name`
+ ORDER BY `number_of_shows` DESC;
