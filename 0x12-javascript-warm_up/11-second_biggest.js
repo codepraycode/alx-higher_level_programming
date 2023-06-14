c86c0faf6192431ece5a25bda6aa0f51b6571828
Precious Olusola <preciousolusola16@gmail.com>
2023-06-14 16:20:16 +0100

    Done with task 11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const argvL = process.argv.length - 2;
+const argv = process.argv;
+let sum = 0;
+
+// A script to print.
+if (argvL > 1) {
+  for (const each of argv.slice(2)) {
+	  sum += parseInt(each);
+  }
+}
+
+console.log(sum);
