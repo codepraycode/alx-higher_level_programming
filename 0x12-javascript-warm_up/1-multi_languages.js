0c29aec77bb3d2907bd59655cfdfd8c04d43f1f1
Precious Olusola <preciousolusola16@gmail.com>
2023-06-14 15:42:17 +0100

    Done with task 1 and 2

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+// A script to print.
+
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const argvL = process.argv.length - 2;
+
+// A script to print.
+if (argvL < 1) {
+  console.log('No argument');
+} else if (argvL === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
