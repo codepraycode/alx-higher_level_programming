c1bb91a6629b4afccf3cecbeb1a10de2f8a912b8
Precious Olusola <preciousolusola16@gmail.com>
2023-06-14 15:50:48 +0100

    Done with task 5

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const argv = process.argv;
+
+// A script to print.
+const num = parseInt(argv[2]);
+
+if (isNaN(num)) {
+  console.log('Not a number');
+} else {
+  console.log('My number:', num);
+}
