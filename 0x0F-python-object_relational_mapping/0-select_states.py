4d57d8ca4cdfb37662429eb56d55ca5e0d0c3632
Precious Olusola <preciousolusola16@gmail.com>
2023-06-23 20:00:13 +0100

    Done with task 0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""A script to select states from DB"""
+import MySQLdb as mysql
+from sys import argv
+
+DB_NAME = "hbtn_0e_0_usa"
+DB_HOST = 'localhost'
+DB_PORT = 3306
+
+
+if __name__ == "__main__":
+    # run when not imported
+    
+    # Read arguments
+    _, user, password, db_name = argv
+    
+    try:
+        db = mysql.connect(host=DB_HOST, port=DB_PORT, user=user,
+                           passwd=password, db=db_name)
+    except Exception as err:
+        print("Failed to connect to database")
+        exit(0)
+    
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states ORDER BY id ASC;")
+
+    result_query = cursor.fetchall()
+
+    for row in result_query:
+        print(row)
+
+    cursor.close()
+    db.close()
