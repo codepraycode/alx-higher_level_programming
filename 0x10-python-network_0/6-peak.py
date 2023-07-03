802bc1acf9fabefc256fe51aac6826637152c7f6
Precious Olusola <51521181+codepraycode@users.noreply.github.com>
2023-07-03 03:13:52 +0100

    Done with mandatory tasks

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Algorithms for list of integers."""
+
+
+def find_peak(list_of_integers):
+    """Function to finds a peak in a list of unsorted integers.
+
+    Args:
+        list_of_integers: [int] - A list fof intergers. 
+    """
+    if list_of_integers:
+        list_of_integers.sort(reverse=True)
+        return list_of_integers[0]
