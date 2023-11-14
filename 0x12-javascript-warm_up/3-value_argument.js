c5aa967f0dafac0926264a37ffa3fdf7f0e0bec5
Precious Olusola <preciousolusola16@gmail.com>
2023-11-14 01:36:32 +0100

    Updated scripts

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,10 +1,7 @@
 #!/usr/bin/node
-const argvL = process.argv.length - 2
-const argv = process.argv
 
-// A script to print.
-if (argvL < 1) {
-  console.log('No argument')
+if (process.argv[2]) {
+  console.log(process.argv[2])
 } else {
-  console.log(argv[2])
+  console.log('No argument')
 }
