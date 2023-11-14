68e934f04b9f145c6b8ce92081aa9b8de2f28a89
Precious Olusola <preciousolusola16@gmail.com>
2023-11-14 01:50:30 +0100

    Fixed semantic error

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -6,8 +6,6 @@ const argv = process.argv
 if (argvL > 1) {
   const ans = [...argv.slice(2)].sort().reverse()[1]
   console.log(ans)
-} else if (argvL === 1) {
-  console.log(1)
 } else {
   console.log(0)
 }
